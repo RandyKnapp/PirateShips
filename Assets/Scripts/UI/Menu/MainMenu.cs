b3f655b34a34953c28603b6cf5949425a1b92f4d
Randy Knapp <randy.bravo2@gmail.com>
2017-02-17 09:43:54 -0800

    Main menu button hookup

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField]
+	private Button playButton;
+
+	private void Start()
+	{
+		playButton.onClick.AddListener(OnPlayButtonClicked);
+	}
+
+	private void OnDestroy()
+	{
+		playButton.onClick.RemoveAllListeners();
+	}
+
+	private void OnPlayButtonClicked()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
